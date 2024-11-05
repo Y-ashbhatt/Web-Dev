68247c12ab5bf869cb05e424a0b6e3ab51eb2d80
Yash Bhatt <0yashbhatt0@gmail.com>
2024-11-05 23:01:07 +0530

    practice

--- Back End/3.5 Secrets Project/index.js
+++ Back End/3.5 Secrets Project/index.js
@@ -6,24 +6,34 @@ import express from "express";
 import { dirname, parse } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+const port = 3001;
+
+var userAuthorised = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function passwordAuthentication(req, res, next) {
+  const password = req.body["password"];
+  if (req.body["password"] == "ILoveProgramming") {
+    userAuthorised = true;
+  } 
+  next();
+}
+
+
+app.use(passwordAuthentication);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/check", (req, res) => {
-   
-  if (req.body["password"]== "ILoveProgramming") {
+  if (userAuthorised) {
     res.sendFile(__dirname + "/public/secret.html");
-  }else{
-    
+  } else {
     res.sendFile(__dirname + "/public/index.html");
   }
 });
